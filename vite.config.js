1ff70fba00364d82307700f35e5ab64c49853f13
giventusmvh <weishen735@gmail.com>
2023-10-25 16:06:19 +0700

    rev

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import legacy from "@vitejs/plugin-legacy";
+
+export default defineConfig({
+  plugins: [
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+      },
+    },
+  },
+});
